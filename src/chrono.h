5c7a2824cc210bb7f88414fd9ce8b6986a385d93
Jasper <jasper.sinclair@mail.com>
2024-01-29 13:01:36 +0100

    update chrono

--- src/chrono.h
+++ src/chrono.h
@@ -1,33 +1,29 @@
 #pragma once
-
 #include <chrono>
 
-#include "bitboard.h"
+#include "main.h"
 
 struct Chrono {
   using TimePoint = std::chrono::milliseconds::rep;
-
   bool stop;
-
   bool use_match_time_limit;
   bool use_node_limit;
   bool use_depth_limit;
   bool use_move_time_limit;
 
-  TimePoint match_time_limit;
   uint64_t node_limit;
   Depth depth_limit;
-  TimePoint move_time_limit;
 
   TimePoint begin;
   TimePoint time_to_use;
+  TimePoint match_time_limit;
+  TimePoint move_time_limit;
   TimePoint time[N_COLORS], inc[N_COLORS];
 
   Chrono();
-
-  static TimePoint now();
   void start();
-  TimePoint elapsed() const;
+  static TimePoint now();
   void InitTimeToUse(Color side_to_move);
   void update(uint64_t node_cnt);
+  [[nodiscard]] TimePoint elapsed() const;
 };

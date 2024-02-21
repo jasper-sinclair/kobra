15a6f32b86e334054a76ab272b4acfe8ecd7fb07
Jasper <jasper.sinclair@mail.com>
2024-02-21 23:35:20 +0100

    refactor
    use Google formatting and naming convention

--- src/attack.h
+++ src/attack.h
@@ -1,273 +1,103 @@
 #pragma once
+#include "bitboard.h"
 
-#include"bitboard.h"
+#ifdef _MSC_VER
+#pragma warning(disable : 4244)
+#else
+#endif
 
-namespace attacks {
-	inline Bitboard pawnAttacks[N_COLORS][N_SQUARES];
-	inline Bitboard knightAttacks[N_SQUARES];
-	inline Bitboard kingAttacks[N_SQUARES];
-	inline int knightDirections[8] = { 17,10,-6,-15,-17,-10,6,15 };
-	inline int kingDirections[8] = { 8,9,1,-7,-8,-9,-1,7 };
-	inline int bishopDirections[4] = { 9,-7,-9,7 };
-	inline int rookDirections[4] = { 8,1,-8,-1 };
+namespace attack {
+inline Bitboard files[8] = {kFileABb, kFileBBb, kFileCBb, kFileDBb,
+                            kFileEBb, kFileFBb, kFileGBb, kFileHBb};
 
-	inline Bitboard files[8] = { FILE_A_BB, FILE_B_BB, FILE_C_BB, FILE_D_BB,
-											FILE_E_BB, FILE_F_BB, FILE_G_BB, FILE_H_BB };
-	inline Bitboard ranks[8] = { RANK_1_BB, RANK_2_BB, RANK_3_BB, RANK_4_BB,
-											RANK_5_BB, RANK_6_BB, RANK_7_BB, RANK_8_BB };
-	inline Bitboard diagonals[15];
-	inline Bitboard antiDiagonals[15];
-	inline Bitboard bishopAttacks[N_SQUARES];
-	inline Bitboard rookAttacks[N_SQUARES];
+inline Bitboard ranks[8] = {kRank1Bb, kRank2Bb, kRank3Bb, kRank4Bb,
+                            kRank5Bb, kRank6Bb, kRank7Bb, kRank8Bb};
 
-	enum Directions {
-		NORTHEAST,
-		NORTH,
-		NORTHWEST,
-		EAST,
-		SOUTHEAST,
-		SOUTH,
-		SOUTHWEST,
-		WEST,
-		N_DIRECTIONS
-	};
-	inline Bitboard rayAttacks[N_DIRECTIONS][N_SQUARES];
+enum Directions {
+  NORTHEAST,
+  NORTH,
+  NORTHWEST,
+  EAST,
+  SOUTHEAST,
+  SOUTH,
+  SOUTHWEST,
+  WEST,
+  N_DIRECTIONS
+};
 
-	inline Bitboard inBetweenSquares[N_SQUARES][N_SQUARES];
+inline int knight_directions[8] = {17, 10, -6, -15, -17, -10, 6, 15};
 
-	inline Bitboard diagonalsBySquare[N_SQUARES];
-	inline Bitboard antiDiagonalsBySquare[N_SQUARES];
-	inline Bitboard filesBySquare[N_SQUARES];
-	inline Bitboard ranksBySquare[N_SQUARES];
-	inline Bitboard firstRankAttacks[8][64];
-	inline Bitboard fillUpAttacks[8][64];
-	inline Bitboard aFileAttacks[8][64];
+inline int king_directions[8] = {8, 9, 1, -7, -8, -9, -1, 7};
 
-	inline void init() {
-		// knight attacks
-		for (Square from = A1; from < N_SQUARES; ++from) {
-			Bitboard attacks = Bitboard();
-			for (const auto& d : knightDirections) {
-        const Square to = from + d;
-				if (square::isValid(to) && square::distance(from, to) <= 2)
-					attacks.set(to);
-			}
-			knightAttacks[from] = attacks;
-		}
+inline int bishop_directions[4] = {9, -7, -9, 7};
 
-		// king attacks
-		for (Square from = A1; from < N_SQUARES; ++from) {
-			Bitboard attacks = Bitboard();
-			for (const auto& d : kingDirections) {
-        const Square to = from + d;
-				if (square::isValid(to) && square::distance(from, to) == 1)
-					attacks.set(to);
-			}
-			kingAttacks[from] = attacks;
-		}
+inline int rook_directions[4] = {8, 1, -8, -1};
 
-		// diagonals
-		diagonals[0] = Bitboard(0x8040201008040201);
-		for (int i = 1; i < 8; ++i) {
-			diagonals[i] = diagonals[0] << i;
-			for (int j = 0; j < i - 1; ++j)
-				diagonals[i] -= ranks[7 - j];
-		}
-		for (int i = 1; i < 8; ++i) {
-			diagonals[i + 7] = diagonals[0] >> i;
-			for (int j = 0; j < i - 1; ++j)
-				diagonals[i + 7] -= ranks[j];
-		}
+inline Bitboard pawn_attacks[N_COLORS][N_SQUARES];
+inline Bitboard knight_attacks[N_SQUARES];
+inline Bitboard king_attacks[N_SQUARES];
 
-		// anti-diagonals
-		for (int i = 0; i < 15; ++i)
-			antiDiagonals[i] = diagonals[i].mirrored();
+inline Bitboard diagonals[15];
+inline Bitboard anti_diagonals[15];
 
-		// bishop attacks on empty board
-		for (Square sq = A1; sq < N_SQUARES; ++sq) {
-			Bitboard attacks = {};
-			for (auto& b : diagonals) {
-				if (b.isSet(sq)) {
-					attacks = b;
-					break;
-				}
-			}
-			for (auto& b : antiDiagonals) {
-				if (b.isSet(sq)) {
-					attacks ^= b;
-					break;
-				}
-			}
-			bishopAttacks[sq] = attacks;
-		}
+inline Bitboard bishop_attacks[N_SQUARES];
+inline Bitboard rook_attacks[N_SQUARES];
+inline Bitboard ray_attacks[N_DIRECTIONS][N_SQUARES];
 
-		// rook attacks on empty board
-		for (Square sq = A1; sq < N_SQUARES; ++sq) {
-			Bitboard attacks = {};
-			for (auto& b : files) {
-				if (b.isSet(sq)) {
-					attacks = b;
-					break;
-				}
-			}
-			for (auto& b : ranks) {
-				if (b.isSet(sq)) {
-					attacks ^= b;
-					break;
-				}
-			}
-			rookAttacks[sq] = attacks;
-		}
+inline Bitboard in_between_squares[N_SQUARES][N_SQUARES];
+inline Bitboard diagonals_by_square[N_SQUARES];
+inline Bitboard anti_diagonals_by_square[N_SQUARES];
+inline Bitboard files_by_square[N_SQUARES];
+inline Bitboard ranks_by_square[N_SQUARES];
 
-		// ray attacks
-		for (Square sq = A1; sq < N_SQUARES; ++sq) {
-      const File file = file::make(sq);
-      const Rank rank = rank::make(sq);
+inline Bitboard first_rank_attacks[8][64];
+inline Bitboard fill_up_attacks[8][64];
+inline Bitboard a_file_attacks[8][64];
 
-			rayAttacks[NORTH][sq] = Bitboard(0x101010101010100) << sq;
-			rayAttacks[SOUTH][sq] = Bitboard(0x80808080808080) >> 63 - sq;
-			rayAttacks[EAST][sq] = Bitboard(0xfe) << sq;
-			if (rank < RANK_8)
-				rayAttacks[EAST][sq] -= ranks[rank + 1];
-			rayAttacks[WEST][sq] = Bitboard(0x7f00000000000000) >> (sq ^ 7 ^ 56);
-			if (rank > RANK_1)
-				rayAttacks[WEST][sq] -= ranks[rank - 1];
-			rayAttacks[NORTHEAST][sq] = Bitboard(0x8040201008040200) << sq;
-			for (int i = 0; i < file - rank - 1; ++i)
-				rayAttacks[NORTHEAST][sq] -= ranks[7 - i];
-			rayAttacks[NORTHWEST][sq] = (Bitboard(0x102040810204000) >> file::make(sq ^ 7)) << 8 * rank;
-			for (int i = 7; i > file; --i)
-				rayAttacks[NORTHWEST][sq] -= files[i];
-			rayAttacks[SOUTHEAST][sq ^ 56] = rayAttacks[NORTHEAST][sq].mirrored();
-			rayAttacks[SOUTHWEST][sq ^ 56] = rayAttacks[NORTHWEST][sq].mirrored();
-		}
+inline Bitboard DiagonalAttacks(const Square sq, Bitboard occ) {
+  occ = diagonals_by_square[sq] & occ;
+  occ = occ * kFileBBb >> 58;
+  return fill_up_attacks[file::make(sq)][occ.data] & diagonals_by_square[sq];
+}
 
-		// pawn attacks
-		for (Square sq = A1; sq < N_SQUARES; ++sq) {
-			Bitboard b = Bitboard::fromSquare(sq);
-			pawnAttacks[WHITE][sq] |= b.shift<7>() | b.shift<9>();
-			pawnAttacks[BLACK][sq] |= b.shift<-9>() | b.shift<-7>();
-		}
+inline Bitboard AntiDiagonalAttacks(const Square sq, Bitboard occ) {
+  occ = anti_diagonals_by_square[sq] & occ;
+  occ = occ * kFileBBb >> 58;
+  return fill_up_attacks[file::make(sq)][occ.data] & anti_diagonals_by_square[sq];
+}
 
-		// squares in between two squares
-		for (Square i = A1; i < N_SQUARES; ++i) {
-			for (Square j = A1; j < N_SQUARES; ++j) {
-        const Bitboard b = Bitboard::fromSquare(j);
-				for (const auto& arr : rayAttacks) {
-					if (arr[i] & b) {
-						inBetweenSquares[i][j] = arr[i] - arr[j] - b;
-						break;
-					}
-				}
-			}
-		}
+inline Bitboard RankAttacks(const Square sq, Bitboard occ) {
+  occ = ranks_by_square[sq] & occ;
+  occ = occ * kFileBBb >> 58;
+  return fill_up_attacks[file::make(sq)][occ.data] & ranks_by_square[sq];
+}
 
-		for (Square i = 0; i < 64; ++i) {
-      const Bitboard b = Bitboard::fromSquare(i);
-			// diagonals by square
-			for (const auto& d : diagonals) {
-				if (d & b) {
-					diagonalsBySquare[i] = d - b;
-					break;
-				}
-			}
-			// anti-diagonals by square
-			for (const auto& d : antiDiagonals) {
-				if (d & b) {
-					antiDiagonalsBySquare[i] = d - b;
-					break;
-				}
-			}
-			// files by square
-			for (const auto& d : files) {
-				if (d & b) {
-					filesBySquare[i] = d - b;
-					break;
-				}
-			}
-			// ranks by square
-			for (const auto& d : ranks) {
-				if (d & b) {
-					ranksBySquare[i] = d - b;
-					break;
-				}
-			}
-		}
+inline Bitboard FileAttacks(const Square sq, Bitboard occ) {
+  occ = kFileABb & occ >> file::make(sq);
+  occ = occ * kDiagC2H7 >> 58;
+  return a_file_attacks[rank::make(sq)][occ.data] << file::make(sq);
+}
 
-		for (Square i = 0; i < 8; ++i) {
-			for (int j = 0; j < 64; ++j) {
+template <PieceType Pt>
+Bitboard attacks(const Square sq, const Bitboard occupied) {
+  switch (Pt) {
+    case BISHOP:
+      return DiagonalAttacks(sq, occupied) | AntiDiagonalAttacks(sq, occupied);
+    case ROOK:
+      return FileAttacks(sq, occupied) | RankAttacks(sq, occupied);
+    case QUEEN:
+      return DiagonalAttacks(sq, occupied) | AntiDiagonalAttacks(sq, occupied) |
+             FileAttacks(sq, occupied) | RankAttacks(sq, occupied);
+    default:;
+  }
+  return {};
+}
 
-				// first rank attacks,
-				Bitboard occ = Bitboard(j<<1) - Bitboard::fromSquare(i);
+template <Color C>
+Bitboard PawnAttacksBb(const Bitboard b) {
+  return C == WHITE ? b.shift<7>() | b.shift<9>()
+                    : b.shift<-9>() | b.shift<-7>();
+}
 
-				for (int k = i-1;; --k) {
-					if (k < 0) break;
-					firstRankAttacks[i][j].set(k);
-					if (occ.isSet(k)) break;
-				}
-				for (int k = i+1;; ++k) {
-					if (k > 7) break;
-					firstRankAttacks[i][j].set(k);
-					if (occ.isSet(k)) break;
-				}
-
-				// fill-up attacks
-				fillUpAttacks[i][j] = FILE_A_BB * firstRankAttacks[i][j];
-
-				// A-file attacks
-        const Square r = i^7;
-				occ = Bitboard(j<<1) - Bitboard::fromSquare(r);
-				for (int k = r-1;; --k) {
-					if (k < 0) break;
-					aFileAttacks[i][j].set(8*(k^7));
-					if (occ.isSet(k)) break;
-				}
-				for (int k = r+1;; ++k) {
-					if (k > 7) break;
-					aFileAttacks[i][j].set(8*(k^7));
-					if (occ.isSet(k)) break;
-				}
-			}
-		}
-	}
-
-	inline Bitboard diagonalAttacks(Square sq, Bitboard occ) {
-		occ = diagonalsBySquare[sq] & occ;
-		occ = occ * FILE_B_BB >> 58;
-		return fillUpAttacks[file::make(sq)][occ.data] & diagonalsBySquare[sq];
-	}
-
-	inline Bitboard antiDiagonalAttacks(Square sq, Bitboard occ) {
-		occ = antiDiagonalsBySquare[sq] & occ;
-		occ = occ * FILE_B_BB >> 58;
-		return fillUpAttacks[file::make(sq)][occ.data] & antiDiagonalsBySquare[sq];
-	}
-
-	inline Bitboard rankAttacks(Square sq, Bitboard occ) {
-		occ = ranksBySquare[sq] & occ;
-		occ = occ * FILE_B_BB >> 58;
-		return fillUpAttacks[file::make(sq)][occ.data] & ranksBySquare[sq];
-	}
-
-	inline Bitboard fileAttacks(Square sq, Bitboard occ) {
-		occ = FILE_A_BB & (occ >> file::make(sq));
-		occ = occ * DIAG_C2_H7 >> 58;
-		return aFileAttacks[rank::make(sq)][occ.data] << file::make(sq);
-	}
-
-	template<PieceType pt>
-	inline Bitboard attacks(Square sq, Bitboard occupied) {
-		switch (pt) {
-		case BISHOP:
-			return diagonalAttacks(sq, occupied) | antiDiagonalAttacks(sq, occupied);
-		case ROOK:
-			return fileAttacks(sq, occupied) | rankAttacks(sq, occupied);
-		case QUEEN:
-			return diagonalAttacks(sq, occupied) | antiDiagonalAttacks(sq, occupied) |
-				fileAttacks(sq, occupied) | rankAttacks(sq, occupied);
-    default: ;
-    }
-		return {};
-	}
-
-} // namespace attacks
+void init();
+}    

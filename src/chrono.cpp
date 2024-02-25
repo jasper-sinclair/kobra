979def69d44824e127f02cfbe46a184236d072bb
Jasper <jasper.sinclair@mail.com>
2024-02-25 03:31:08 +0100

    Update chrono.cpp

--- src/chrono.cpp
+++ src/chrono.cpp
@@ -7,8 +7,8 @@ Chrono::Chrono() { std::memset(this, 0, sizeof(Chrono)); }
 
 Chrono::TimePoint Chrono::now() {
   return std::chrono::duration_cast<std::chrono::milliseconds>(
-             std::chrono::steady_clock::now().time_since_epoch())
-      .count();
+    std::chrono::steady_clock::now().time_since_epoch())
+    .count();
 }
 
 void Chrono::start() { begin = now(); }
@@ -20,20 +20,21 @@ void Chrono::InitTimeToUse(const Color side_to_move) {
     use_match_time_limit = true;
     constexpr TimePoint overhead = 50;
     time_to_use =
-        (match_time_limit < inc[side_to_move]
-        ? match_time_limit : 0.05
-        * (match_time_limit - inc[side_to_move])
-        + inc[side_to_move])
-        - overhead;
+      (match_time_limit < inc[side_to_move]
+      ? match_time_limit : 0.05
+      * (match_time_limit - inc[side_to_move])
+      + inc[side_to_move])
+      - overhead;
     time_to_use = std::max(time_to_use, static_cast<TimePoint>(10));
-  } else
+  }
+  else
     time_to_use = std::numeric_limits<long long>::max();
 }
 
 void Chrono::update(const uint64_t node_cnt) {
   if (use_match_time_limit && elapsed() > time_to_use ||
-      use_node_limit && node_cnt > node_limit ||
-      use_move_time_limit && elapsed() > move_time_limit) {
+    use_node_limit && node_cnt > node_limit ||
+    use_move_time_limit && elapsed() > move_time_limit) {
     stop = true;
   }
 }

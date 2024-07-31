1ab4cc4281c7b64ad75365419b78d675c80550e4
Jasper <jasper.sinclair@mail.com>
2024-07-31 15:06:30 +0200

    update movesort

--- src/movesort.cpp
+++ src/movesort.cpp
@@ -13,14 +13,14 @@
 #endif
 
 MoveSort::MoveSort(Board& board, Stack* ss, Histories& histories,
-                   const Move hash_move, const bool is_in_check)
+  const Move hash_move, const bool is_in_check)
   : board(board),
-    histories(histories),
-    is_in_check(is_in_check),
-    idx{},
-    stage(HASH_MOVE),
-    hash_move(hash_move),
-    ss(ss) {
+  histories(histories),
+  is_in_check(is_in_check),
+  idx{},
+  stage(HASH_MOVE),
+  hash_move(hash_move),
+  ss(ss) {
   assert(!histories.killer[ss->ply][0] ||
     histories.killer[ss->ply][0] != histories.killer[ss->ply][1]);
 }
@@ -44,12 +44,12 @@ Move MoveSort::next() {
     ComputeScores();
     std::ranges::sort(moves, [](const MoveData& m1, const MoveData& m2) {
       return m1.score > m2.score;
-    });
+      });
     [[fallthrough]];
   case NORMAL:
     if (static_cast<size_t>(idx) == moves.size()) return Move();
     return moves.move(idx++);
-  default: ;
+  default:;
   }
   return 0;
 }
@@ -66,8 +66,8 @@ void MoveSort::ComputeScores() {
   const Bitboard threatened_pieces =
     board.color(us) &
     (threatened_by_pawn & (board.pieces(KNIGHT) | board.pieces(BISHOP)) |
-      threatened_by_minor & board.pieces(ROOK) |
-      threatened_by_rook & board.pieces(QUEEN));
+    threatened_by_minor & board.pieces(ROOK) |
+    threatened_by_rook & board.pieces(QUEEN));
 
   for (auto& [move, score] : moves) {
     const Move m = move;
@@ -78,9 +78,9 @@ void MoveSort::ComputeScores() {
       const Piece moved = board.PieceOn(move::from(m));
       const Square to =
         move::move_type(m) == move::EN_PASSANT
-          ? move::to(m) -
-          static_cast<Square>(direction::PawnPush(board.side_to_move))
-          : move::to(m);
+        ? move::to(m) -
+        static_cast<Square>(direction::PawnPush(board.side_to_move))
+        : move::to(m);
       const PieceType captured = piece_type::make(board.PieceOn(to));
       s += 89 * eval::kPieceValues[board.PieceOn(move::to(m))] +
         histories.capture[moved][to][captured];

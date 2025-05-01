e09043e5cce877c8d0ed8c983040087edc02efd6
Jasper <jasper.sinclair@mail.com>
2025-05-02 00:37:45 +0200

    update uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -1,177 +1,161 @@
 #include "uci.h"
-
-#include <ostream>
-
-#include "eval.h"
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
 #include "movegen.h"
+#include "nnue.h"
 
-int uci::loop() {
-  const char* filename = evalfile.c_str();
-  nnue_init(filename);
-  constexpr int ret = 0;
-  board = Board(kStartFen);
-  search.SetHashSize(kDefaultHash);
-  search.SetNumThreads(kDefaultThreads);
-  std::string line, token;
+void uci::init(){
+  use_nnue=true;
+  (void)nnue::instance();
+  pos=board(start_fen);
+  search.set_hash_size(default_hash);
+  search.set_num_threads(default_threads);
+  loop();
+}
 
-  for (;;) {
-    std::getline(std::cin, line);
-    auto ss = std::istringstream(line);
-    token.clear();
-    ss >> token;
-    if (token == "quit") {
-      stop();
+void uci::loop(){
+  std::string line,token;
+  std::unordered_map<std::string,std::function<void(std::istringstream&)>> commands={
+
+  {"uci",[](std::istringstream&) {info(); }},
+  {"ucinewgame",[](std::istringstream&) {newgame(); }},
+  {"setoption",[](std::istringstream& ss) {setoption(ss); }},
+  {"isready",[](std::istringstream&) {SO<<"readyok"<<NL; }},
+  {"position",[](std::istringstream& ss) {position(ss); }},
+  {"go",[](const std::istringstream& ss) {go(ss.str()); }},
+  {"stop",[](std::istringstream&) {stop(); }},
+  {"quit",[](std::istringstream&) {stop(); exit(0); }},
+  {"print",[](std::istringstream&) {SO << pos << NL << pos.fen() << NL; }},
+  {"perft",perft}};
+
+  while(std::getline(std::cin,line)){
+	    std::istringstream ss(line);
+    ss>>token;
+    if(auto it=commands.find(token);it!=commands.end()){it->second(ss);}
+  }
+}
+
+void uci::info(){
+  SO<<"id name "<<engname<<" "<<version<<NL;
+  SO<<"id author "<<author<<NL;
+  for(const auto& [name, type, default_value, min_value, max_value]:ucioptions){
+    SO<<"option name "<<name<<" type "<<type<<" default "<<default_value<<" min "<<min_value<<" max "<<max_value<<NL;
+  }
+  SO<<"uciok"<<SE;
+}
+
+void uci::newgame(){
+  search.clear();
+}
+
+void uci::setoption(std::istringstream& ss){
+  std::string token,name,value;
+  bool reading_name=false;
+  bool reading_value=false;
+  while(ss>>token){
+    if(token=="name"){
+      reading_name=true;
+      reading_value=false;
+    } else if(token=="value"){
+      reading_name=false;
+      reading_value=true;
+    } else if(reading_name){
+      if(!name.empty()) name+=" ";
+      name+=token;
+    } else if(reading_value){
+      if(!value.empty()) value+=" ";
+      value+=token;
+    }
+  }
+  if(name=="Hash"){
+    const u64 hash_size=std::stoull(value);
+    search.hash.set_size(hash_size);
+  } else if(name=="Threads"){
+    search.set_num_threads(std::stoi(value));
+  } else if(name=="Contempt"){
+    contempt=std::stoi(value);
+  } else if(name=="UseNNUE"){
+    use_nnue=value=="true"||value=="1";
+    SO<<"Set UseNNUE to "<<(use_nnue?"true":"false")<<SE;
+  } else{
+    SO<<"Unknown option: "<<name<<" with value: "<<value<<SE;
+  }
+}
+
+void uci::position(std::istringstream& ss){
+  std::string token,fen;
+  ss>>token;
+  if(token=="startpos"){
+    fen=start_fen;
+    ss>>token;
+  } else if(token=="fen"){
+    while(ss>>token&&token!="moves") fen+=token+" ";
+  } else return;
+  pos=board(fen);
+  while(ss>>token){
+    if(const u16 move=to_move(token,pos);move){
+      pos.apply_move(move);
+    } else{
+      std::cerr<<"Invalid move: "<<token<<NL;
       break;
     }
-    if (token == "stop")
-      stop();
-    else if (token == "uci")
-      uci();
-    else if (token == "ucinewgame")
-      ucinewgame();
-    else if (token == "setoption")
-      setoption(ss);
-    else if (token == "isready") {
-      std::cout << "readyok" << std::endl;
-    }
-    else if (token == "position")
-      position(ss);
-    else if (token == "go")
-      go(ss.str());
-    else if (token == "d") {
-      std::cout << board;
-      std::cout << board.fen() << std::endl;
-    }
-    else if (token == "perft") {
-      Depth depth;
-      ss >> depth;
-      const std::chrono::steady_clock::time_point begin =
-        std::chrono::steady_clock::now();
-      const uint64_t node_cnt = perft(board, depth);
-      const std::chrono::steady_clock::time_point end =
-        std::chrono::steady_clock::now();
-      std::cout << "node " << node_cnt << std::endl;
-      std::cout << "time " << static_cast<double>((end - begin).count()) * 1e-9
-        << std::endl;
-    }
-  }
-  return ret;
-}
-
-int uci::uci() {
-  std::cout << "id name kobra 1.2" << '\n';
-  std::cout << "id author jasper" << '\n';
-  std::cout << "option name Hash type spin default " << kDefaultHash
-    << " min 1 max " << kMaxHashSize << '\n';
-  std::cout << "option name Threads type spin default " << kDefaultThreads
-    << " min 1 max " << kMaxThreads << '\n';
-  std::cout << "uciok" << '\n';
-  return fflush(stdout);
-}
-
-void uci::ucinewgame() { search.clear(); }
-
-void uci::setoption(std::istringstream& ss) {
-  std::string token;
-  while (ss >> token) {
-    if (token == "name") {
-      ss >> token;
-      if (token == "Hash") {
-        ss >> token;
-        if (token == "value") {
-          uint64_t hash_size;
-          ss >> hash_size;
-          if (hash_size) search.hash.SetSize(hash_size);
-        }
-      }
-      else if (token == "Threads") {
-        ss >> token;
-        if (token == "value") {
-          ss >> token;
-          const ThreadId count = static_cast<ThreadId>(std::stoi(token));
-          search.SetNumThreads(count);
-        }
-      }
-    }
   }
 }
 
-int uci::position(std::istringstream& ss) {
-  constexpr int ret = 0;
-  std::string token, fen;
-  ss >> token;
-  if (token == "startpos") {
-    fen = kStartFen;
-    ss >> token;
-  }
-  else if (token == "fen") {
-    while (ss >> token && token != "moves") fen += token + " ";
-  }
-  else
-    return 0;
-  board = { fen };
-
-  while (ss >> token) {
-    const Move move = ToMove(token, board);
-    if (!move) {
-      std::cout << "Invalid move: " << token << std::endl;
-      break;
-    }
-    board.ApplyMove(move);
-  }
-  return ret;
-}
-
-void uci::go(const std::string& str) {
+void uci::go(const std::string& str){
+  std::scoped_lock lock(search_mutex);
   stop();
-  search.time = {};
+  search.time={};
   search.time.start();
   std::istringstream ss(str);
   std::string token;
-
-  while (ss >> token) {
-    if (token == "wtime")
-      ss >> search.time.time[WHITE];
-    else if (token == "btime")
-      ss >> search.time.time[BLACK];
-    else if (token == "winc")
-      ss >> search.time.inc[WHITE];
-    else if (token == "binc")
-      ss >> search.time.inc[BLACK];
-    else if (token == "nodes") {
-      search.time.use_node_limit = true;
-      ss >> search.time.node_limit;
-    }
-    else if (token == "depth") {
-      search.time.use_depth_limit = true;
-      ss >> search.time.depth_limit;
-    }
-    else if (token == "movetime") {
-      search.time.use_move_time_limit = true;
-      ss >> search.time.move_time_limit;
+  while(ss>>token){
+    if(token=="wtime") ss>>search.time.time[white];
+    else if(token=="btime") ss>>search.time.time[black];
+    else if(token=="winc") ss>>search.time.inc[white];
+    else if(token=="binc") ss>>search.time.inc[black];
+    else if(token=="nodes"){
+      search.time.use_node_limit=true;
+      ss>>search.time.node_limit;
+    } else if(token=="depth"){
+      search.time.use_depth_limit=true;
+      ss>>search.time.depth_limit;
+    } else if(token=="movetime"){
+      search.time.use_move_limit=true;
+      ss>>search.time.move_time_limit;
     }
   }
-
-  search.time.InitTimeToUse(board.side_to_move);
-  thread = std::thread(GetBestMove);
+  search.time.init_time(pos.side_to_move);
+  thread=std::jthread(get_bestmove);
 }
 
-void uci::stop() {
+void uci::stop(){
   search.stop();
-  if (thread.joinable()) thread.join();
+  if(thread.joinable()) thread.join();
 }
 
-Move uci::ToMove(const std::string& str, Board& pos) {
-  MoveList moves;
-  GenMoves(pos, moves);
-  for (const auto& [move, score] : moves) {
-    if (move::ToString(move) == str) return move;
+void uci::perft(std::istringstream& ss){
+  i32 depth;
+  ss>>depth;
+  const auto begin=std::chrono::steady_clock::now();
+  const auto node_cnt=perft(pos,depth);
+  const auto end=std::chrono::steady_clock::now();
+  SO<<"node "<<node_cnt<<NL;
+  SO<<"time "<<std::chrono::duration<double>(end-begin).count()<<NL;
+}
+
+u16 uci::to_move(const std::string& str,board& b){
+  move_list moves;
+  gen_moves(b,moves);
+  for(const auto& [move, score]:moves){
+    if(move::move_to_string(move)==str) return move;
   }
-  return Move();
+  return 0;
 }
 
-int uci::GetBestMove() {
-  const Move best = search.BestMove(board);
-  std::cout << "bestmove " << move::ToString(best) << '\n';
-  return fflush(stdout);
+void uci::get_bestmove(){
+  const u16 move=search.best_move(pos);
+  SO<<"bestmove "<<move::move_to_string(move)<<SE;
 }
--- src/uci.h
+++ src/uci.h
@@ -1,26 +1,46 @@
 #pragma once
+#include <mutex>
 #include <thread>
-
 #include "search.h"
 
-namespace uci {
-  constexpr size_t kDefaultHash = 256;
-  constexpr ThreadId kDefaultThreads = 1;
+struct option{
+  std::string name;
+  std::string type;
+  int default_value;
+  int min_value;
+  int max_value;
+};
 
-  inline Board board;
-  inline Search search;
-  inline std::string evalfile = "kobra_1.2.nnue";
-  inline std::thread thread;
-
-  int loop();
-  int position(std::istringstream& ss);
-  int GetBestMove();
-  int uci();
-
-  Move ToMove(const std::string& str, Board& pos);
-
+namespace uci{
+  inline bool use_nnue=true;
+  constexpr int default_contempt=1;
+  constexpr size_t default_hash=256;
+  constexpr thread_id default_threads=1;
+  inline board pos;
+  inline const std::string start_fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  inline int contempt=default_contempt;
+  inline search_info search;
+  inline std::jthread thread;
+  inline std::mutex search_mutex;
+  inline std::vector<option> ucioptions={
+  {.name="Hash",.type="spin",.default_value=default_hash,.min_value=1,.max_value=max_hash_size},
+  {.name="Threads",.type="spin",.default_value=default_threads,.min_value=1,.max_value=max_threads},
+  {.name="Contempt",.type="spin",.default_value=default_contempt,.min_value=-100,.max_value=100},
+  {.name="UseNNUE",.type="check",.default_value=true,.min_value=0,.max_value=1}
+  };
+  u16 to_move(const std::string& str,board& b);
+  void get_bestmove();
   void go(const std::string& str);
+  void info();
+  void init();
+  void loop();
+  void newgame();
+  void perft(std::istringstream& ss);
+  void position(std::istringstream& ss);
   void setoption(std::istringstream& ss);
   void stop();
-  void ucinewgame();
 }
+
+inline std::string engname="kobra";
+inline std::string version="2.0";
+inline std::string author="jasper";

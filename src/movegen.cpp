9264d1ffb91ba9f27cbd6141ead4386d147a2d91
Jasper <jasper.sinclair@mail.com>
2026-02-27 17:43:29 +0100

    update movegen

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -2,147 +2,156 @@
 #include <algorithm>
 #include "attack.h"
 
-template<bool C> void gen_pawn_moves(board& pos,move_list& movelist){
+template <bool C> void gen_pawn_moves(
+  board& pos,
+  move_list& movelist){
   u8 from;
   u8 to;
-  constexpr i32 up_left=C==white?northwest:southwest;
-  constexpr i32 up_right=C==white?northeast:southeast;
-  constexpr i32 up=C==white?north:south;
-  constexpr bool them=!C;
-  constexpr bool us=C;
-  constexpr bitboard relative_rank4_bb=C==white?rank4:rank5;
-  constexpr bitboard relative_rank8_bb=C==white?rank8:rank1;
-  bitboard empty=~pos.occupied();
-  const bitboard our_pawns=pos.get_pieces(us,pawn);
-  const bitboard single_pawn_push_targets=our_pawns.shift<up>()&empty;
-  const bitboard their_team=pos.get_color(them);
-  const bitboard up_left_bb=our_pawns.shift<up_left>();
-  const bitboard up_left_captures=up_left_bb&their_team;
-  const bitboard up_right_bb=our_pawns.shift<up_right>();
-  const bitboard up_right_captures=up_right_bb&their_team;
-  bitboard atts=single_pawn_push_targets-relative_rank8_bb;
-  while(atts){
-    to=pop_lsb(atts);
-    from=to-SCU8(up);
+  constexpr i32 up_left = C == white?northwest:southwest;
+  constexpr i32 up_right = C == white?northeast:southeast;
+  constexpr i32 up = C == white?north:south;
+  constexpr bool them = ! C;
+  constexpr bool us = C;
+  constexpr bitboard relative_rank4_bb = C == white?rank4:rank5;
+  constexpr bitboard relative_rank8_bb = C == white?rank8:rank1;
+  bitboard empty = ~pos.occupied();
+  const bitboard our_pawns = pos.get_pieces(us,pawn);
+  const bitboard single_pawn_push_targets = our_pawns.shift<up>() & empty;
+  const bitboard their_team = pos.get_color(them);
+  const bitboard up_left_bb = our_pawns.shift<up_left>();
+  const bitboard up_left_captures = up_left_bb & their_team;
+  const bitboard up_right_bb = our_pawns.shift<up_right>();
+  const bitboard up_right_captures = up_right_bb & their_team;
+  bitboard atts = single_pawn_push_targets - relative_rank8_bb;
+  while (atts){
+    to = pop_lsb(atts);
+    from = to - SCU8(up);
     movelist.add(move::make(from,to));
   }
-  atts=single_pawn_push_targets&relative_rank8_bb;
-  while(atts){
-    to=pop_lsb(atts);
-    from=to-SCU8(up);
+  atts = single_pawn_push_targets & relative_rank8_bb;
+  while (atts){
+    to = pop_lsb(atts);
+    from = to - SCU8(up);
     movelist.add(move::make<knight>(from,to));
     movelist.add(move::make<bishop>(from,to));
     movelist.add(move::make<rook>(from,to));
     movelist.add(move::make<queen>(from,to));
   }
-  atts=single_pawn_push_targets.shift<up>()&empty&relative_rank4_bb;
-  while(atts){
-    to=pop_lsb(atts);
-    from=to-SCU8(2*up);
+  atts = single_pawn_push_targets.shift<up>() & empty & relative_rank4_bb;
+  while (atts){
+    to = pop_lsb(atts);
+    from = to - SCU8(2 * up);
     movelist.add(move::make(from,to));
   }
-  atts=up_right_captures-relative_rank8_bb;
-  while(atts){
-    to=pop_lsb(atts);
-    from=to-SCU8(up_right);
+  atts = up_right_captures - relative_rank8_bb;
+  while (atts){
+    to = pop_lsb(atts);
+    from = to - SCU8(up_right);
     movelist.add(move::make(from,to));
   }
-  atts=up_left_captures-relative_rank8_bb;
-  while(atts){
-    to=pop_lsb(atts);
-    from=to-SCU8(up_left);
+  atts = up_left_captures - relative_rank8_bb;
+  while (atts){
+    to = pop_lsb(atts);
+    from = to - SCU8(up_left);
     movelist.add(move::make(from,to));
   }
-  atts=up_right_captures&relative_rank8_bb;
-  while(atts){
-    to=pop_lsb(atts);
-    from=to-SCU8(up_right);
+  atts = up_right_captures & relative_rank8_bb;
+  while (atts){
+    to = pop_lsb(atts);
+    from = to - SCU8(up_right);
     movelist.add(move::make<knight>(from,to));
     movelist.add(move::make<bishop>(from,to));
     movelist.add(move::make<rook>(from,to));
     movelist.add(move::make<queen>(from,to));
   }
-  atts=up_left_captures&relative_rank8_bb;
-  while(atts){
-    to=pop_lsb(atts);
-    from=to-SCU8(up_left);
+  atts = up_left_captures & relative_rank8_bb;
+  while (atts){
+    to = pop_lsb(atts);
+    from = to - SCU8(up_left);
     movelist.add(move::make<knight>(from,to));
     movelist.add(move::make<bishop>(from,to));
     movelist.add(move::make<rook>(from,to));
     movelist.add(move::make<queen>(from,to));
   }
-  if(pos.st->ep_sq!=no_sq){
-    atts=up_right_bb&bitboard::from_sq(pos.st->ep_sq);
-    if(atts){
-      to=pos.st->ep_sq;
-      from=to-SCU8(up_right);
+  if (pos.st->ep_sq != no_sq){
+    atts = up_right_bb & bitboard::from_sq(pos.st->ep_sq);
+    if (atts){
+      to = pos.st->ep_sq;
+      from = to - SCU8(up_right);
       movelist.add(make(from,to,move::en_passant));
     }
-    atts=up_left_bb&bitboard::from_sq(pos.st->ep_sq);
-    if(atts){
-      to=pos.st->ep_sq;
-      from=to-SCU8(up_left);
+    atts = up_left_bb & bitboard::from_sq(pos.st->ep_sq);
+    if (atts){
+      to = pos.st->ep_sq;
+      from = to - SCU8(up_left);
       movelist.add(make(from,to,move::en_passant));
     }
   }
 }
 
-template<bool C,i32 Pt> void gen_piece_moves(board& pos,move_list& movelist){
-  bitboard friendly=pos.get_color(C);
-  bitboard get_pieces=pos.get_pieces(C,Pt);
-  while(get_pieces){
-    u8 from=pop_lsb(get_pieces);
-    bitboard atts=
-      (Pt==knight
+template <bool C, i32 Pt> void gen_piece_moves(
+  board& pos,
+  move_list& movelist){
+  bitboard friendly = pos.get_color(C);
+  bitboard get_pieces = pos.get_pieces(C,Pt);
+  while (get_pieces){
+    u8 from = pop_lsb(get_pieces);
+    bitboard atts =
+      (Pt == knight
         ?attack::knight_att[from]
-        :attack::atts<Pt>(from,pos.occupied()))-
+        :attack::atts<Pt>(from,pos.occupied())) -
       friendly;
-    while(atts) movelist.add(move::make(from,pop_lsb(atts)));
+    while (atts) movelist.add(move::make(from,pop_lsb(atts)));
   }
 }
 
-template<bool C> void gen_king_moves(board& pos,move_list& movelist){
-  const u8 ksq=pos.ksq(C);
-  bitboard atts=attack::king_att[ksq]-pos.get_color(C);
-  while(atts) movelist.add(move::make(ksq,pop_lsb(atts)));
-  if(ksq==relative(C,e1)){
-    const bitboard empty=~pos.occupied();
-    constexpr bitboard path1=
-      C==white?white_qs_path:black_qs_path;
-    constexpr bitboard path2=
-      C==white?white_ks_path:black_ks_path;
-    if(pos.can_castle(C==white?white_qs:black_qs)&&
-      (empty&path1)==path1)
+template <bool C> void gen_king_moves(
+  board& pos,
+  move_list& movelist){
+  const u8 ksq = pos.ksq(C);
+  bitboard atts = attack::king_att[ksq] - pos.get_color(C);
+  while (atts) movelist.add(move::make(ksq,pop_lsb(atts)));
+  if (ksq == relative(C,e1)){
+    const bitboard empty = ~pos.occupied();
+    constexpr bitboard path1 =
+      C == white?white_qs_path:black_qs_path;
+    constexpr bitboard path2 =
+      C == white?white_ks_path:black_ks_path;
+    if (pos.can_castle(C == white?white_qs:black_qs) &&
+      (empty & path1) == path1)
       movelist.add(make(ksq,relative(C,c1),move::castle));
-    if(pos.can_castle(C==white?white_ks:black_ks)&&
-      (empty&path2)==path2)
+    if (pos.can_castle(C == white?white_ks:black_ks) &&
+      (empty & path2) == path2)
       movelist.add(make(ksq,relative(C,g1),move::castle));
   }
 }
 
-void gen_moves(board& pos,move_list& movelist){
-  if(!pos.st->king_attacinfo.computed) pos.gen_king_attack_info(pos.st->king_attacinfo);
-  movelist.last=movelist.data;
-  if(pos.side_to_move==white){
-    if(!pos.st->king_attacinfo.double_check){
+void gen_moves(
+  board& pos,
+  move_list& movelist){
+  if (! pos.st->king_attacinfo.computed) pos.gen_king_attack_info(pos.st->king_attacinfo);
+  movelist.last = movelist.data;
+  if (pos.side_to_move == white){
+    if (! pos.st->king_attacinfo.double_check){
       gen_pawn_moves<white>(pos,movelist);
-      gen_piece_moves<white,knight>(pos,movelist);
-      gen_piece_moves<white,bishop>(pos,movelist);
-      gen_piece_moves<white,rook>(pos,movelist);
-      gen_piece_moves<white,queen>(pos,movelist);
+      gen_piece_moves<white, knight>(pos,movelist);
+      gen_piece_moves<white, bishop>(pos,movelist);
+      gen_piece_moves<white, rook>(pos,movelist);
+      gen_piece_moves<white, queen>(pos,movelist);
     }
     gen_king_moves<white>(pos,movelist);
   } else{
-    if(!pos.st->king_attacinfo.double_check){
+    if (! pos.st->king_attacinfo.double_check){
       gen_pawn_moves<black>(pos,movelist);
-      gen_piece_moves<black,knight>(pos,movelist);
-      gen_piece_moves<black,bishop>(pos,movelist);
-      gen_piece_moves<black,rook>(pos,movelist);
-      gen_piece_moves<black,queen>(pos,movelist);
+      gen_piece_moves<black, knight>(pos,movelist);
+      gen_piece_moves<black, bishop>(pos,movelist);
+      gen_piece_moves<black, rook>(pos,movelist);
+      gen_piece_moves<black, queen>(pos,movelist);
     }
     gen_king_moves<black>(pos,movelist);
   }
-  movelist.last=std::ranges::remove_if(movelist,[&](const move_info& m){
-    return !pos.is_legal(m.move);
-  }).begin();
+  movelist.last = std::ranges::remove_if(movelist,[&](
+    const move_info& m){
+      return ! pos.is_legal(m.move);
+    }).begin();
 }
--- src/movegen.h
+++ src/movegen.h
@@ -1,16 +1,18 @@
 #pragma once
 #include <cstring>
 #include <iostream>
+#include <string>
 #include "bitboard.h"
 
-constexpr int max_moves=256;
+constexpr int max_moves = 256;
 
 struct move_info{
   u16 move;
   int score;
 
-  bool operator==(const u16 mv) const{
-    return this->move==mv;
+  bool operator==(
+    const u16 mv) const{
+    return this->move == mv;
   }
 };
 
@@ -19,75 +21,100 @@ struct move_list{
     return last;
   }
 
-  [[nodiscard]] int score(const size_t idx) const{
+  [[nodiscard]] int score(
+    const size_t idx) const{
     return data[idx].score;
   }
 
-  [[nodiscard]] u16 move(const size_t idx) const{
+  [[nodiscard]] u16 move(
+    const size_t idx) const{
     return data[idx].move;
   }
 
   [[nodiscard]] size_t size() const{
-    return last-data;
+    return last - data;
   }
 
-  move_info data[max_moves],* last;
+  move_info data[max_moves], *last;
 
   move_info* begin(){
     return data;
   }
 
-  friend std::ostream& operator<<(std::ostream& os,const move_list& movelist);
+  friend std::ostream& operator<<(
+    std::ostream& os,
+    const move_list& movelist);
 
-  void add(const u16 move){
-    last++->move=move;
+  void add(
+    const u16 move){
+    last++->move = move;
   }
 
-  void remove(size_t idx);
+  void remove(
+    size_t idx);
+
   move_list() : data{}, last(data){}
 };
 
-inline void move_list::remove(const size_t idx){
-  std::memmove(begin()+idx,begin()+idx+1,
-    (size()-idx-1)*sizeof(move_info));
+inline void move_list::remove(
+  const size_t idx){
+  std::memmove(begin() + idx,begin() + idx + 1,
+    (size() - idx - 1) * sizeof(move_info));
   --last;
 }
 
-template<bool C> void gen_pawn_moves(board& pos,move_list& movelist);
-template<bool C,i32 Pt> void gen_piece_moves(board& pos,move_list& movelist);
-template<bool C> void gen_king_moves(board& pos,move_list& movelist);
-void gen_moves(board& pos,move_list& movelist);
+template <bool C> void gen_pawn_moves(
+  board& pos,
+  move_list& movelist);
 
-template<bool Root=true> u64 perft(board& pos,const int depth){
-  u64 cnt,nodes=0;
-  const bool leaf=depth==2;
+template <bool C, i32 Pt> void gen_piece_moves(
+  board& pos,
+  move_list& movelist);
+
+template <bool C> void gen_king_moves(
+  board& pos,
+  move_list& movelist);
+
+void gen_moves(
+  board& pos,
+  move_list& movelist);
+
+template <bool Root = true> u64 perft(
+  board& pos,
+  const int depth){
+  u64 cnt, nodes = 0;
+  const bool leaf = depth == 2;
   move_list moves;
   gen_moves(pos,moves);
-  for(size_t i=0;i<moves.size();++i){
-    if(Root&&depth<=1){
-      cnt=1;
+  for (size_t i = 0; i < moves.size(); ++i){
+    if (Root && depth <= 1){
+      cnt = 1;
       ++nodes;
     } else{
       pos.apply_move(moves.move(i));
       move_list m;
       gen_moves(pos,m);
-      cnt=leaf?m.size():perft<false>(pos,depth-1);
-      nodes+=cnt;
+      cnt = leaf?m.size():perft<false>(pos,depth - 1);
+      nodes += cnt;
       pos.undo_move();
     }
-    if(Root){
-      SO<<move::move_to_string(moves.move(i))<<" "<<cnt<<SE;
+    if (Root){
+      SO << move::move_to_string(moves.move(i)) << " " << cnt << SE;
     }
   }
   return nodes;
 }
 
-inline std::ostream& operator<<(std::ostream& os,const move_list& movelist){
-  for(int i=0;i<SCI(movelist.size());++i) os<<move::move_to_string(movelist.move(i))<<" ";
+inline std::ostream& operator<<(
+  std::ostream& os,
+  const move_list& movelist){
+  for (size_t i = 0; i < movelist.size(); ++i)
+    os << move::move_to_string(movelist.move(static_cast<int>(i))) << " ";
   return os;
 }
 
-inline void print(const std::vector<u16>& v){
-  for(const auto& m:v)
-    SO<<move::move_to_string(m)<<" "<<SE;
+inline void print(
+  const std::vector<u16>& v){
+  for (const auto& m : v)
+    SO << move::move_to_string(m) << " " << SE;
 }

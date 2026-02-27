f008660d2c9832f2ebc32931f2814a0a75a927d5
Jasper <jasper.sinclair@mail.com>
2026-02-27 08:49:30 +0100

    update chrono

--- src/chrono.cpp
+++ src/chrono.cpp
@@ -3,11 +3,6 @@
 #include "main.h"
 #include "uci.h"
 
-#ifdef _MSC_VER
-#pragma warning(disable : 4244)
-#else
-#endif
-
 chrono::chrono(){
   std::memset(this,0,sizeof(chrono));
 }
@@ -19,34 +14,36 @@ time_point chrono::now(){
 }
 
 void chrono::start(){
-  begin=now();
+  begin = now();
 }
 
 time_point chrono::elapsed() const{
-  return now()-begin;
+  return now() - begin;
 }
 
-void chrono::init_time(const bool side_to_move){
-  match_time_limit=time[side_to_move];
-  if(match_time_limit){
-    use_match_limit=true;
-    constexpr time_point overhead=30;
-    time_to_use=match_time_limit/20+inc[side_to_move];
-    if(match_time_limit<2000){
-      time_to_use=std::max(time_to_use,SCTP(100));
+void chrono::init_time(
+  const bool side_to_move){
+  match_time_limit = time[side_to_move];
+  if (match_time_limit){
+    use_match_limit = true;
+    constexpr time_point overhead = 30;
+    time_to_use = match_time_limit / 20 + inc[side_to_move];
+    if (match_time_limit < 2000){
+      time_to_use = std::max(time_to_use,SCTP(100));
     } else{
-      time_to_use=std::max(time_to_use,SCTP(500));
+      time_to_use = std::max(time_to_use,SCTP(500));
     }
-    time_to_use-=overhead;
+    time_to_use -= overhead;
   } else{
-    time_to_use=std::numeric_limits<int64_t>::max();
+    time_to_use = std::numeric_limits<int64_t>::max();
   }
 }
 
-void chrono::update(const u64 node_cnt){
-  if(use_match_limit&&elapsed()>time_to_use||
-    use_node_limit&&node_cnt>node_limit||
-    use_move_limit&&elapsed()>move_time_limit){
-    stop=true;
+void chrono::update(
+  const u64 node_cnt){
+  if ((use_match_limit && elapsed() > time_to_use) ||
+    (use_node_limit && node_cnt > node_limit) ||
+    (use_move_limit && elapsed() > move_time_limit)){
+    stop = true;
   }
 }
--- src/chrono.h
+++ src/chrono.h
@@ -10,18 +10,25 @@ struct chrono{
   bool use_node_limit;
 
   chrono();
-  i32 depth_limit;
 
+  i32 depth_limit;
   time_point begin;
   time_point match_time_limit;
   time_point move_time_limit;
   time_point time_to_use;
-  time_point time[n_colors],inc[n_colors];
+  time_point time[n_colors], inc[n_colors];
+
   [[nodiscard]] time_point elapsed() const;
+
   static time_point now();
+
   u64 node_limit;
 
-  void init_time(bool side_to_move);
+  void init_time(
+    bool side_to_move);
+
   void start();
-  void update(u64 node_cnt);
+
+  void update(
+    u64 node_cnt);
 };

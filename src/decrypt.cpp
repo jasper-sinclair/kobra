4e66a8fe1154041d9c47941de877547f2eac02be
Jasper <jasper.sinclair@mail.com>
2026-03-02 18:08:16 +0100

    Update decrypt.cpp

--- src/decrypt.cpp
+++ src/decrypt.cpp
@@ -1,34 +1,17 @@
+#include "decrypt.h"
 #include <cstring>
 #include <vector>
-
-extern "C" {
-#include "../../crypto/aes.h"
-}
-
-#include "decrypt.h"
 #include "key.h"
 
 std::vector<uint8_t> decrypt(
   const uint8_t* blob,
-  const size_t blob_size) {
+  size_t blob_size) {
   uint8_t key[32];
   derive_key(key);
-  const uint8_t* iv=blob;
-  const uint8_t* ciphertext=blob + 16;
-  const size_t size=blob_size - 16;
 
-  std::vector decrypted(
-    ciphertext,
-    ciphertext + size);
+  std::vector<uint8_t> decrypted(blob, blob + blob_size);
 
-  AES_ctx ctx;
-  AES_init_ctx_iv(&ctx, key, iv);
-  AES_CTR_xcrypt_buffer(
-    &ctx,
-    decrypted.data(),
-    static_cast<uint32_t>(size));
-
-  memset(key, 0, sizeof(key));
+  xor_stream(decrypted.data(), decrypted.size(), key);
 
   return decrypted;
 }

1641abac33d140b5fb93bc7fe56e5f8da048ded9
Jasper <jasper.sinclair@mail.com>
2026-03-02 01:16:05 +0100

    Create decrypt.cpp

---
+++ src/decrypt.cpp
@@ -0,0 +1,35 @@
+#include <cstring>
+#include <vector>
+
+extern "C" {
+#include "../../shared/crypto/aes.h"
+}
+
+#include "decrypt.h"
+#include "crypto_key.h"
+
+std::vector<uint8_t> decrypt_blob(
+  const uint8_t* blob,
+  const size_t blob_size) {
+  uint8_t key[32];
+  derive_key(key);
+
+  const uint8_t* iv=blob;
+  const uint8_t* ciphertext=blob + 16;
+  const size_t size=blob_size - 16;
+
+  std::vector decrypted(
+    ciphertext,
+    ciphertext + size);
+
+  AES_ctx ctx;
+  AES_init_ctx_iv(&ctx, key, iv);
+  AES_CTR_xcrypt_buffer(
+    &ctx,
+    decrypted.data(),
+    static_cast<uint32_t>(size));
+
+  memset(key, 0, sizeof(key));
+
+  return decrypted;
+}

1f453abbd0b0d36c3431cac03533170d4cb29f85
jasper-sinclair <jasper.sinclair@mail.com>
2026-07-10 14:35:27 +0200

    repo cleanup

--- src/decrypt.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdint>
-#include <vector>
-
-std::vector<uint8_t> decrypt(
-  const uint8_t* blob,
-  size_t blob_size);
--- src/key.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <cstdint>
-void derive_key(uint8_t key[32]);
-void xor_stream(uint8_t* data, size_t size, const uint8_t key[32]);
